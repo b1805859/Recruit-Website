c9b93263ee7dac3845b7a19b1f29db629f669c13
b1805859 <ngokinh1234567@gmail.com>
2022-05-13 23:28:09 +0700

    update code

--- Models/Jobs.js
+++ Models/Jobs.js
@@ -3,26 +3,28 @@ const Schema = mongoose.Schema;
 var slug = require('mongoose-slug-generator');
 mongoose.plugin(slug);
 
-
-const Jobs = new Schema({
-    namecity: { type: String},
-    namejob:{type:String},
-    email:{type:String},
-    number: {type: Number},
+const Jobs = new Schema(
+  {
+    namecity: { type: String },
+    namejob: { type: String },
+    email: { type: String },
+    number: { type: Number },
     location: { type: String },
-    skill: { type: String},
-    keyword: {type: String},
+    skill: { type: String },
+    keyword: { type: String },
     description: { type: String },
-    slug: {type: String, slug:"name"},
-    time: {type: String},
-    id_user:{type:String},
-    user_apply:[
-        {   userEmail:{type: String},
-            userNumber:{type: Number},
-            userLinkCV:{type: String},
-        }
-    ]
-}, {timestamps: true}
+    slug: { type: String, slug: 'name' },
+    money: { type: String },
+    id_user: { type: String },
+    user_apply: [
+      {
+        userEmail: { type: String },
+        userNumber: { type: Number },
+        userLinkCV: { type: String },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Jobs', Jobs);
