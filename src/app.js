8f325b34bc56a19174d5f5519db894dc6ed13ab8
b1805859 <ngokinh1234567@gmail.com>
2022-05-15 05:31:03 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -1,42 +1,40 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var path = require('path');
 var app = express();
-const bp = require('body-parser')
+const bp = require('body-parser');
 
-var methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-const port = process.env.PORT;
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '/public')));
-var router = require('./routes/index.js')
-var connect = require('./config/db/index.js')
-var cookieParser = require('cookie-parser')
+var router = require('./routes/index.js');
+var connect = require('./config/db/index.js');
+var cookieParser = require('cookie-parser');
 
-var hbs = exphbs.create({extname: '.hbs',
+var hbs = exphbs.create({
+  extname: '.hbs',
   helpers: {
-      sum(a,b) {return a+b}
-  }
+    sum(a, b) {
+      return a + b;
+    },
+  },
 });
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 //De truoc router
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-
-
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 connect();
 router(app);
 
-
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});
