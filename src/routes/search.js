d5853ffacb9344936eb2d120813280dfdb19a211
b1805859 <ngokinh1234567@gmail.com>
2022-05-15 17:32:43 +0700

    make it better

--- src/routes/search.js
+++ src/routes/search.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const searchControllers = require('../../Controllers/JobControllers');
 
-router.get('/contact', searchControllers.contact);
-router.get('/security', searchControllers.security);
 router.get('/:id/detail', searchControllers.detail);
 router.get('/:id/apply', searchControllers.apply);
 router.put('/:id/applied', searchControllers.applied);
 router.get('/post', searchControllers.post);
 router.post('/post', searchControllers.stored);
 router.post('/jobs', searchControllers.home);
+router.get('/contact', searchControllers.contact);
+router.get('/security', searchControllers.security);
 router.get('/jobs', searchControllers.homeheader);
 
 module.exports = router;
