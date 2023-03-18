53cac851ded746b1f3c9f64d5ad89d6bfc89d1f5
b1805859 <ngokinh30311@gmail.com>
2023-03-18 22:57:17 +0700

    update code

--- Controllers/JobControllers.js
+++ Controllers/JobControllers.js
@@ -1,99 +1,110 @@
-const Jobs = require('../Models/Jobs');
-const {
-  mutipleMongooseToObject,
-  singleToObject,
-} = require('../src/util/mongoose');
 
-class JobControllers {
-  homeheader(req, res, next) {
+const Jobs = require('../Models/Jobs')
+const {mutipleMongooseToObject, singleToObject} = require('../src/util/mongoose')
+
+class JobControllers
+{
+   
+
+  homeheader(req,res, next)
+  {
     Jobs.find({})
-      .then((Jobs) => {
-        res.render('search/jobs', {
-          Jobs: mutipleMongooseToObject(Jobs),
-          username: req.user.name,
-        });
+      .then(Jobs=>{
+        res.render('search/jobs',{Jobs: mutipleMongooseToObject(Jobs),username: req.user.name});
       })
-      .catch(next);
+      .catch(next)
   }
 
   home(req, res, next) {
-    return req.body.keyword
-      ? Jobs.find({
-          keyword: {
-            $regex: '.*' + `${req.body.keyword}` + '.*',
-            $options: 'i',
-          },
-          location: req.body.location,
+    
+    
+    return req.body.keyword ?
+    ( 
+      Jobs.find({keyword:{ $regex: '.*' + `${req.body.keyword}`+ '.*',$options: 'i'}, location:req.body.location})
+      .then(Jobs=>{
+        res.render('search/jobs',{Jobs: mutipleMongooseToObject(Jobs),username: req.user.name});
+      })
+      .catch(next)
+  
+    ) : 
+    (
+      Jobs.find({ location:req.body.location})
+      .then(Jobs=>{
+        res.render('search/jobs',{Jobs: mutipleMongooseToObject(Jobs),username: req.user.name});
+      })
+      .catch(next)
+    )
+
+  
+    }
+
+
+
+    detail(req, res, next)
+    {
+      Jobs.findById({_id: req.params.id})
+        .then(Job=>{
+          res.render('search/detail',{Job: singleToObject(Job),username: req.user.name});
         })
-          .then((Jobs) => {
-            res.render('search/jobs', {
-              Jobs: mutipleMongooseToObject(Jobs),
-              username: req.user.name,
-            });
-          })
-          .catch(next)
-      : Jobs.find({ location: req.body.location })
-          .then((Jobs) => {
-            res.render('search/jobs', {
-              Jobs: mutipleMongooseToObject(Jobs),
-              username: req.user.name,
-            });
-          })
-          .catch(next);
-  }
+      
+    }
 
-  detail(req, res, next) {
-    Jobs.findById({ _id: req.params.id }).then((Job) => {
-      res.render('search/detail', {
-        Job: singleToObject(Job),
-        username: req.user.name,
-      });
-    });
-  }
+    
+    apply(req, res, next)
+    {
+      Jobs.findOne({_id: req.params.id})
+        .then(Job => {
+            res.render('search/apply',{Job: singleToObject(Job),username: req.user.name});
+        })
 
-  apply(req, res, next) {
-    Jobs.findOne({ _id: req.params.id }).then((Job) => {
-      res.render('search/apply', {
-        Job: singleToObject(Job),
-        username: req.user.name,
-      });
-    });
-  }
+          
+      
+    }
 
-  applied(req, res) {
-    Jobs.updateOne(
-      { _id: req.params.id },
-      {
-        $push: {
-          user_apply: {
-            userEmail: req.body.userEmail,
-            userNumber: req.body.userNumber,
-            userLinkCV: req.body.linkCV,
-          },
-        },
-      },
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(400).send('Error');
-        } else {
-          res.redirect('/search/jobs');
-        }
-      }
-    );
-  }
+    applied(req,res)
+    {
+      
+      
+        Jobs.updateOne(
+          {_id : req.params.id},
+          {
+            $push: {
+              user_apply:{
+                userEmail : req.body.userEmail,
+                userNumber : req.body.userNumber,
+                userLinkCV: req.body.linkCV
+              }
+            }
+          }, 
+          (err,result)=>{
+            if(err){
+              console.log(err);
+              res.status(400).send('Error')
+            }else{
+              res.redirect('/search/jobs')
+            }
+          }
+        );
+    }
 
-  post(req, res) {
-    res.render('search/post', { username: req.user.name });
-  }
 
-  stored(req, res) {
-    const job = req.body;
-    job.id_user = req.user._id;
-    const jobs = new Jobs(job);
-    jobs.save();
-    res.redirect('/search/jobs');
-  }
+
+    post(req, res)
+    {
+      res.render('search/post',{username: req.user.name});
+    }
+
+
+    stored(req, res)
+    { 
+      const job = req.body
+      job.id_user = req.user._id
+      const jobs = new Jobs(job);
+      jobs.save()
+      res.redirect("/search/jobs");
+    }
+
+
 }
 
 module.exports = new JobControllers();
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+
 async function connect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connection sucessfully');
-  } catch {
-    console.log('Connection failue');
-  }
+    try{
+        await mongoose.connect('mongodb://localhost:27017/PTPMTN');
+        console.log("Connection sucessfully");
+    }
+    catch
+    {
+        console.log("Connection failue");
+    }
 }
+
 
 module.exports = connect;
